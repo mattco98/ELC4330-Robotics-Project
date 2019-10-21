1117c2b79fa81da0a13da0c924dd36ab687798ef
Matthew Olsson <matthewcolsson@gmail.com>
2019-10-21 13:15:05 -0500

    Reformat solidworks and finished drawing
    - Added SolidWorks assemblies to the git repo
    - Mated SolidWorks assemblies to the origin, removing the need for the
    baseNormTransforms field
    - Fix light position so it doesn't change on every plot
    - Added gripper separation functionality

--- src/dh_table.m
+++ src/dh_table.m
@@ -1,11 +1,16 @@
-function table = dh_table(thetas)
+function table = dh_table(thetas, grip)
     if ~all(size(thetas) == [1 5])
         error('Input for dh_table function must be a 1x5 column vector.\n\tInput vector size: %dx%d', size(thetas, 1), size(thetas, 2));
     end
     
-    table(1) = struct('theta', thetas(1), 'd', 6.51,   'a', 0,       'alpha', pi / 2);
-    table(2) = struct('theta', thetas(2), 'd', 0,      'a', 11.811,  'alpha', 0);
-    table(3) = struct('theta', thetas(3), 'd', 0,      'a', 12.4968, 'alpha', 0);
-    table(4) = struct('theta', thetas(4), 'd', 0,      'a', 0,       'alpha', pi / 2);
-    table(5) = struct('theta', thetas(5), 'd', 8.5598, 'a', 0,       'alpha', 0);
+    table(1) = struct('theta', thetas(1), 'd', 6.51,   'a', 0,         'alpha', pi / 2);
+    table(2) = struct('theta', thetas(2), 'd', 0,      'a', 11.811,    'alpha', 0);
+    table(3) = struct('theta', thetas(3), 'd', 0,      'a', 12.4968,   'alpha', 0);
+    table(4) = struct('theta', thetas(4), 'd', 0,      'a', 0,         'alpha', pi / 2);
+    table(5) = struct('theta', thetas(5), 'd', 4.1846, 'a', 0,         'alpha', 0);
+    
+    % NOTE: The next two entries aren't really a part of the formal DH
+    % table, they're just here to help with drawing the gripper parts
+    table(6) = struct('theta', pi / 2,    'd', 0,      'a', grip / 2,  'alpha', 0);
+    table(7) = struct('theta', 0,         'd', 0,      'a', -grip,     'alpha', 0);
 end
