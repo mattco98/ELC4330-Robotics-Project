bade3b6baacb76badd1813ea066928b8ad5c4fae
Matthew Olsson <matthewcolsson@gmail.com>
2019-10-28 15:36:34 -0500

    Add test script

--- test.m
+++ test.m
@@ -1,17 +1,16 @@
-syms t1 t2 t3 t4 t5 grip
+angles = [
+    rand() * pi - pi / 2
+    rand() * pi
+    rand() * -pi / 2
+    rand() * 6 * pi / 8 - pi / 4
+    rand() * 2 * pi - pi
+]';
 
-dh = dh_table([t1 t2 t3 t4 t5], grip);
+L5draw_group2(angles, 3.55)
 
-trans = Transformation.b();
+ee = L5forward_group2(angles);
+inv = L5inverse_group2(ee);
 
-for i = 4:5
-    trans = trans.dh(dh(i).theta, dh(i).d, dh(i).a, dh(i).alpha);
-end
-
-simplify(vpa(trans.m(), 5))
-
-%     table(1) = struct('theta', thetas(1), 'd', 8.415,  'a', 0,         'alpha', pi / 2);
-%     table(2) = struct('theta', thetas(2), 'd', 0,      'a', 11.811,    'alpha', 0);
-%     table(3) = struct('theta', thetas(3), 'd', 0,      'a', 12.4968,   'alpha', 0);
-
-% vpa(Transformation.b().dh(t1, 8.415, 0, pi / 2).dh(t2, 0, 11.811, 0).dh(t3, 0, 12.4968, 0).m(), 5)
+fprintf("Angles:         %8.3f %8.3f %8.3f %8.3f %8.3f\n", angles * 180 / pi);
+fprintf("EE:             %8.3f %8.3f %8.3f %8.3f %8.3f\n", [ee(1:3), ee(4:5) * 180 / pi]);
+fprintf("Inverse angles: %8.3f %8.3f %8.3f %8.3f %8.3f\n", inv * 180 / pi);
